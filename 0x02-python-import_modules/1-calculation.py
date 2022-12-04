ecb3b64b2d311fae085e5c19bbc4ede2085a2299
AtuaheneBarimah <110355559+AtuaheneBarimah@users.noreply.github.com>
2022-12-04 22:02:25 +0000

    Update 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,8 +6,13 @@ if __name__ == "__main__":
 
     a = 10
     b = 5
+    
+    aa = add(a, b)
+    bb = sub(a, b)
+    cc =  mul(a, b)
+    dd = div(a, b)
 
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
+    print("{} + {} = {}".format(a, b, aa))
+    print("{} - {} = {}".format(a, b, bb))
+    print("{} * {} = {}".format(a, b, cc))
+    print("{} / {} = {}".format(a, b, dd))
